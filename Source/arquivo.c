5c514d8b32c892ba89e5d5d6ecb31a1a41409e5e
Antonio Beserra <44549201+AntonioBeserra@users.noreply.github.com>
2020-03-24 16:27:52 -0300

    Add files via upload

---
+++ Source/arquivo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+	// opens a file "teste.txt" for writing at the end of it or create a new one if it does not exist
+	FILE * f = fopen("teste.txt", "wa");
+	// write some content into the file
+	fputs("Hello world!", f);
+	// close the file
+	fclose(f);
+
+	return 0;
+}
